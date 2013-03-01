d340eaf05083cd0556f0de8d8ec6ff29e1278059
Zeex <zeex@rocketmail.com>
2013-03-01 21:19:22 +0700

    Append .py to the main script's filename

--- bin/samp-server-cli.bat
+++ bin/samp-server-cli.bat
@@ -1,2 +1,2 @@
-@python %~dp0\samp-server-cli %*%
+@python %~dp0\samp-server-cli.py %*%
 
--- bin/samp-server-cli.ps1
+++ bin/samp-server-cli.ps1
@@ -1,4 +1,4 @@
 $scriptPath = $MyInvocation.MyCommand.Definition
 $scriptName = $MyInvocation.MyCommand.Name
-$cliScriptPath = $scriptPath.Replace($scriptName, "") + "samp-server-cli"
+$cliScriptPath = $scriptPath.Replace($scriptName, "") + "samp-server-cli.py"
 python $cliScriptPath $args
