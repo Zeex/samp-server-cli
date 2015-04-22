b779dffdd692319475581f0fde99f26ac73283be
Zeex <zeex@rocketmail.com>
2015-04-22 20:39:06 +0600

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name='samp-server-cli',
-  version='0.1',
+  version='0.1.1',
   author='Zeex',
   author_email='zeex@rocketmail.com',
   url='https://github.com/Zeex/samp-server-cli',
