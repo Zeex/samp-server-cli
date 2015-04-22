33723ae53c63be3e88b3f8ea31cbb9b501c89d6c
Zeex <zeex@rocketmail.com>
2015-04-22 21:00:42 +0600

    v0.1.2

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 
 setup(
   name='samp-server-cli',
-  version='0.1.1',
+  version='0.1.2',
   author='Zeex',
   author_email='zeex@rocketmail.com',
   url='https://github.com/Zeex/samp-server-cli',
   description='A command line interface for SA-MP server',
   license='BSD',
+  py_modules = ['samp_server_cli'],
   entry_points = {
-    'console_scripts': ['samp-server-cli=samp-server-cli:main']
+    'console_scripts': ['samp-server-cli=samp_server_cli:main']
   },
 )
