c0a37649fed4e12fbcb3eef38cee57aa9e8cac03
Zeex <zeex@rocketmail.com>
2019-04-28 15:21:01 +0600

    v1.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
   name='samp-server-cli',
-  version='0.1.9',
+  version='1.0',
   author='Zeex',
   author_email='zeex@rocketmail.com',
   url='https://github.com/Zeex/samp-server-cli',
-  description='A command line interface for SA-MP server',
+  description='Advanced CLI for GTA: San Andreas Multiplayer (SA-MP) server',
   license='BSD',
   py_modules = ['samp_server_cli'],
   entry_points = {
